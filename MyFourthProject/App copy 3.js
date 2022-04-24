b100d403320fd9354b21857f8a3d03e58a336fe3
asung116 <89489179+asung116@users.noreply.github.com>
2022-04-24 22:35:33 +0000

    Updated Project 4
    List View & Section List

--- MyFourthProject/App copy 3.js
+++ MyFourthProject/App copy 3.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Text, Image, ScrollView } from 'react-native';
 
-export default function MyScrollViewApp() {
-  const dog = {
+const dog = {
     url: 'https://user-images.githubusercontent.com/89489179/164997645-f08fafe2-fb49-4c38-b356-823c799d4973.jpg',
     width: 64,
     height: 64
-  };
-  return (
+};
+
+export default MyScrollViewApp = () => {
+  
     <ScrollView style={{padding: 40}}>
       <Text style={{fontsize: 80}}> Try to scroll down</Text>
-      <Image source={dog} />
+      <Image source={require('./assets/dog.jpg')} style={{width: 80, height: 80}} />
       <Image source={dog} />
       <Image source={dog} />
       <Image source={dog} />
@@ -66,6 +67,4 @@ export default function MyScrollViewApp() {
       <Image source={dog} />
       <Image source={dog} />
     </ScrollView>
-  );
 }
-
--- MyFourthProject/App.js
+++ MyFourthProject/App.js
@@ -1,71 +1,27 @@
 import React from 'react';
-import { Text, Image, ScrollView } from 'react-native';
+import { Text, View, SectionList } from 'react-native';
 
-export default function MyScrollViewApp() {
-  const dog = {
-    url: 'https://user-images.githubusercontent.com/89489179/164997645-f08fafe2-fb49-4c38-b356-823c799d4973.jpg',
-    width: 64,
-    height: 64
-  };
+export default StatesApp = () => {
   return (
-    <ScrollView style={{padding: 40}}>
-      <Text style={{fontsize: 80}}> Try to scroll down</Text>
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Text style={{fontsize: 80}}> Try to scroll down again, if you like</Text>
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Text style={{fontsize: 80}}> Try to scroll down again, if you like</Text>
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Text style={{fontsize: 80}}> Try to scroll down again, if you like</Text>
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Text style={{fontsize: 80}}> Try to scroll down again, if you like</Text>
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Text style={{fontsize: 80}}> Try to scroll down again, if you like</Text>
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Text style={{fontsize: 80}}> Try to scroll down again, if you like</Text>
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-    </ScrollView>
+    <View style={{flex: 1, paddingTop: 22}}>
+      <SectionList
+       sections={[
+          {title: 'A-C', data: ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut']},
+          {title: 'D-H', data: ['Delaware', 'Florida', 'Georgia', 'Hawaii']},
+          {title: 'I-L', data: ['Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana']},
+          {title: 'M', data: ['Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minesota', 'Mississippi', 'Misouri', 'Montana']},
+          {title: 'N', data: ['Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota']},
+          {title: 'O-R', data: ['Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island']},
+          {title: 'S-U', data: ['South Carolina', 'South Dakota', 'Tennesse', 'Texas', 'Utah']},
+          {title: 'V-W', data: ['Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']}
+
+        ]}
+        renderItem={({item}) => <Text style={{padding: 10, fontSize: 20, height: 44}}> {item} </Text>}
+        renderSectionHeader={({section}) => <Text style={{paddingTop: 4, paddingLeft:10,
+        paddingRight: 10, paddingBottom: 4, fontSize: 14, fontWeight: 'bold',
+        backgroundColor: '#9FA8DA',}}>{section.title}</Text>}
+        keyExtractor={(item,index) =>index}
+      />
+    </View>
   );
 }
-
